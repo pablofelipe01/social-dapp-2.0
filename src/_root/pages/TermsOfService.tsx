cf8dcc04dad4ca8c284c44dd090c4d2d868519d3
Pablo Acebedo <pablofelipe@me.com>
2024-04-17 08:45:48 -0500

    terms and policies

--- src/_root/pages/TermsOfService.tsx
+++ src/_root/pages/TermsOfService.tsx
@@ -1,109 +1,112 @@
 // import React from 'react';
 
 const TermsOfService = () => {
-  return (
-    <div className="terms-container p-5 md:p-8 lg:p-14 text-white bg-dark-1">
-      <h1 className="h1-bold mb-5">Terms of Service for Tok Sol.io</h1>
-      <p className="body-medium">
-        Welcome to Tok Sol.io! By using Tok Sol.io, you agree to these terms, which create a binding legal agreement between you and Tok Sol.io, Inc. These terms apply to all users of the Tok Sol.io platform and its services, including our website, Web3 applications, APIs, and other digital services.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Your Account</h2>
-      <p className="body-medium">
-        You must be at least 13 years old to use Tok Sol.io. By creating an account, you confirm that you are at least 13 years old and that you are responsible for your account’s activities. You must provide accurate information and keep this information up to date. To create or use an account on Tok Sol.io, you must also be old enough to consent to the processing of your personal data in your jurisdiction or have permission from your parent or guardian if required.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Content and Creations</h2>
-      <p className="body-medium">
-        As a creator on Tok Sol.io, you may establish a creator page to engage with users who support your digital content through subscriptions or one-time purchases. You retain ownership of the content you post, subject to our platform’s right to use and display your content as part of our services.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Payments and Subscriptions</h2>
-      <p className="body-medium">
-        Creators may offer their content on a one-time or subscription basis. Prices may be displayed in the user's local currency and adjusted for blockchain transaction fees as determined by the network and Tok Sol.io’s discretion. Creators are responsible for complying with tax regulations and for setting realistic expectations within their offerings.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Intellectual Property</h2>
-      <p className="body-medium">
-        Creators are responsible for ensuring they have all the necessary rights to the content they upload to Tok Sol.io. The platform respects intellectual property laws and expects the same from its users.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Prohibited Conduct</h2>
-      <p className="body-medium">
-        Users must not engage in illegal activities, abuse the platform’s technical infrastructure, or conduct themselves in a manner that could be deemed as abusive or harmful to other users or the platform.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Termination and Suspension</h2>
-      <p className="body-medium">
-        Tok Sol.io reserves the right to suspend or terminate accounts that violate these terms or engage in activities harmful to the platform or its users.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Changes to Terms</h2>
-      <p className="body-medium">
-        We may modify these terms at any time. We will notify users of any material changes through our platform interfaces or via email.
-      </p>
-
-      <h2 className="h2-bold mt-5 mb-3">Contact Us</h2>
-      <p className="body-medium mb-5">
-        If you have any questions or concerns about these terms or our services, please contact us at pablo@toksol.io
-      </p>
-    </div>
-    <br />
-    <div className="privacy-container p-5 md:p-8 lg:p-14 text-white bg-dark-1">
-      <h1 className="h1-bold mb-5">Privacy Policy for True Social Token NFT Subscription Service</h1>
-      <p className="body-medium mb-4">
-        Effective Date: October 4, 2023
-      </p>
-      <h2 className="h2-bold mt-5 mb-3">Welcome to True Social Token!</h2>
-      <p className="body-medium">
-        True Social Token is a platform dedicated to connecting collectors with unique NFTs through a subscription model. "True Social Token," "we," "us," and "our" refer to TOKSOL.IO, this platform, and the services offered by us, including our website, mobile applications, and other digital services. This Privacy Policy is part of our Terms of Use and describes how we collect, use, and share information.
-      </p>
-      <h2 className="h2-bold mt-5 mb-3">Information You Provide to True Social Token</h2>
-      <p className="body-medium">
-        This is information that you provide directly to us through various means such as account creation, subscription purchases, and direct communications. The types of information we typically collect include:
-      </p>
-      <ul className="list-disc ml-5 mb-3">
-        <li>First and last name</li>
-        <li>Email address</li>
-        <li>Phone number</li>
-        <li>Payment information (processed via secure third-party payment services)</li>
-        <li>Wallet address for NFT transactions</li>
-        <li>Username and password</li>
-        <li>Country and state/province of residence</li>
-      </ul>
-      <h2 className="h2-bold mt-5 mb-3">Automatically Collected Information</h2>
-      <p className="body-medium">
-        When you interact with our Services, we automatically collect certain information about your device and your usage of our services, including:
-      </p>
-      <ul className="list-disc ml-5 mb-3">
-        <li>IP address</li>
-        <li>Device and connection information</li>
-        <li>Location data (derived from IP address)</li>
-        <li>Browser type and settings</li>
-        <li>Information about your activity on our Services</li>
-      </ul>
-      <h2 className="h2-bold mt-5 mb-3">How We Use Your Information</h2>
-      <p className="body-medium">
-        We use the information we collect to provide and manage your subscription and transactions, improve and personalize our Services, communicate with you about your account and updates to our services, conduct analytics and performance monitoring, and comply with legal obligations.
-      </p>
-      <h2 className="h2-bold mt-5 mb-3">Sharing Your Information</h2>
-      <p className="body-medium">
-        We share information with service providers who help us provide our Services, such as payment processors and cloud hosting services. If required by law, we may also disclose your information to authorities or in legal proceedings.
-      </p>
-      <h2 className="h2-bold mt-5 mb-3">Your Rights and Choices</h2>
-      <p className="body-medium">
-        You have certain rights regarding the information we hold about you. You can access, correct, or delete your personal information, change your account settings, and opt out of certain data processing activities.
-      </p>
-      <h2 className="h2-bold mt-5 mb-3">Changes to This Privacy Policy</h2>
-      <p className="body-medium">
-        We may update this policy from time to time to reflect changes to our practices or for other operational, legal, or regulatory reasons. We will notify you of any material changes by posting the new policy on our website or through other communication channels.
-      </p>
-      <h2 className="h2-bold mt-5 mb-3">Contact Us</h2>
-      <p className="body-medium mb-5">
-        If you have any questions about this privacy policy, or our practices, or your dealings with our services, please contact us at pablo@toksol.io.
-      </p>
-    </div>
-  );
-}
-
-export default TermsOfService;
+    return (
+      <>
+        <div className="terms-container p-5 md:p-8 lg:p-14 text-white bg-dark-1">
+          <h1 className="h1-bold mb-5">Terms of Service for Tok Sol.io</h1>
+          <p className="body-medium">
+            Welcome to Tok Sol.io! By using Tok Sol.io, you agree to these terms, which create a binding legal agreement between you and Tok Sol.io, Inc. These terms apply to all users of the Tok Sol.io platform and its services, including our website, Web3 applications, APIs, and other digital services.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Your Account</h2>
+          <p className="body-medium">
+            You must be at least 13 years old to use Tok Sol.io. By creating an account, you confirm that you are at least 13 years old and that you are responsible for your account’s activities. You must provide accurate information and keep this information up to date. To create or use an account on Tok Sol.io, you must also be old enough to consent to the processing of your personal data in your jurisdiction or have permission from your parent or guardian if required.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Content and Creations</h2>
+          <p className="body-medium">
+            As a creator on Tok Sol.io, you may establish a creator page to engage with users who support your digital content through subscriptions or one-time purchases. You retain ownership of the content you post, subject to our platform’s right to use and display your content as part of our services.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Payments and Subscriptions</h2>
+          <p className="body-medium">
+            Creators may offer their content on a one-time or subscription basis. Prices may be displayed in the user's local currency and adjusted for blockchain transaction fees as determined by the network and Tok Sol.io’s discretion. Creators are responsible for complying with tax regulations and for setting realistic expectations within their offerings.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Intellectual Property</h2>
+          <p className="body-medium">
+            Creators are responsible for ensuring they have all the necessary rights to the content they upload to Tok Sol.io. The platform respects intellectual property laws and expects the same from its users.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Prohibited Conduct</h2>
+          <p className="body-medium">
+            Users must not engage in illegal activities, abuse the platform’s technical infrastructure, or conduct themselves in a manner that could be deemed as abusive or harmful to other users or the platform.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Termination and Suspension</h2>
+          <p className="body-medium">
+            Tok Sol.io reserves the right to suspend or terminate accounts that violate these terms or engage in activities harmful to the platform or its users.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Changes to Terms</h2>
+          <p className="body-medium">
+            We may modify these terms at any time. We will notify users of any material changes through our platform interfaces or via email.
+          </p>
+  
+          <h2 className="h2-bold mt-5 mb-3">Contact Us</h2>
+          <p className="body-medium mb-5">
+            If you have any questions or concerns about these terms or our services, please contact us at pablo@toksol.io
+          </p>
+        </div>
+        <br />
+        <div className="privacy-container p-5 md:p-8 lg:p-14 text-white bg-dark-1">
+          <h1 className="h1-bold mb-5">Privacy Policy for True Social Token NFT Subscription Service</h1>
+          <p className="body-medium mb-4">
+            Effective Date: October 4, 2023
+          </p>
+          <h2 className="h2-bold mt-5 mb-3">Welcome to True Social Token!</h2>
+          <p className="body-medium">
+            True Social Token is a platform dedicated to connecting collectors with unique NFTs through a subscription model. "True Social Token," "we," "us," and "our" refer to TOKSOL.IO, this platform, and the services offered by us, including our website, mobile applications, and other digital services. This Privacy Policy is part of our Terms of Use and describes how we collect, use, and share information.
+          </p>
+          <h2 className="h2-bold mt-5 mb-3">Information You Provide to True Social Token</h2>
+          <p className="body-medium">
+            This is information that you provide directly to us through various means such as account creation, subscription purchases, and direct communications. The types of information we typically collect include:
+          </p>
+          <ul className="list-disc ml-5 mb-3">
+            <li>First and last name</li>
+            <li>Email address</li>
+            <li>Phone number</li>
+            <li>Payment information (processed via secure third-party payment services)</li>
+            <li>Wallet address for NFT transactions</li>
+            <li>Username and password</li>
+            <li>Country and state/province of residence</li>
+          </ul>
+          <h2 className="h2-bold mt-5 mb-3">Automatically Collected Information</h2>
+          <p className="body-medium">
+            When you interact with our Services, we automatically collect certain information about your device and your usage of our services, including:
+          </p>
+          <ul className="list-disc ml-5 mb-3">
+            <li>IP address</li>
+            <li>Device and connection information</li>
+            <li>Location data (derived from IP address)</li>
+            <li>Browser type and settings</li>
+            <li>Information about your activity on our Services</li>
+          </ul>
+          <h2 className="h2-bold mt-5 mb-3">How We Use Your Information</h2>
+          <p className="body-medium">
+            We use the information we collect to provide and manage your subscription and transactions, improve and personalize our Services, communicate with you about your account and updates to our services, conduct analytics and performance monitoring, and comply with legal obligations.
+          </p>
+          <h2 className="h2-bold mt-5 mb-3">Sharing Your Information</h2>
+          <p className="body-medium">
+            We share information with service providers who help us provide our Services, such as payment processors and cloud hosting services. If required by law, we may also disclose your information to authorities or in legal proceedings.
+          </p>
+          <h2 className="h2-bold mt-5 mb-3">Your Rights and Choices</h2>
+          <p className="body-medium">
+            You have certain rights regarding the information we hold about you. You can access, correct, or delete your personal information, change your account settings, and opt out of certain data processing activities.
+          </p>
+          <h2 className="h2-bold mt-5 mb-3">Changes to This Privacy Policy</h2>
+          <p className="body-medium">
+            We may update this policy from time to time to reflect changes to our practices or for other operational, legal, or regulatory reasons. We will notify you of any material changes by posting the new policy on our website or through other communication channels.
+          </p>
+          <h2 className="h2-bold mt-5 mb-3">Contact Us</h2>
+          <p className="body-medium mb-5">
+            If you have any questions about this privacy policy, or our practices, or your dealings with our services, please contact us at pablo@toksol.io.
+          </p>
+        </div>
+      </>
+    );
+  }
+  
+  export default TermsOfService;
+  
